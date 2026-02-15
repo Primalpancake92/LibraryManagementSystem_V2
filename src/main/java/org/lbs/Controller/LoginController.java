07dc854aed16b62f27351df705cf969bfd10df5c
Primalpancake92 <johnle.jl39@gmail.com>
2026-02-15 11:43:21 +1100

    LoginController commit

--- src/main/java/org/lbs/Controller/LoginController.java
+++ src/main/java/org/lbs/Controller/LoginController.java
@@ -1,4 +1,15 @@
 package org.lbs.Controller;
 
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import org.lbs.Model.application.User;
+
 public class LoginController {
+    private User model;
+    private Stage stage;
+
+    @FXML
+    public void intialize() {
+
+    }
 }
