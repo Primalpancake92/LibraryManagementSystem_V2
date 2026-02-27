4913d03b149ac1a0dfe08bb7f41219d990575748
Primalpancake92 <johnle.jl39@gmail.com>
2026-02-28 08:52:44 +1100

    registerView change commit:
    Changes:
       - Added button top margins
       - Added some CSS styling to the buttons
       - Fixed the button clipping issue with the input fields.

--- src/main/java/org/lbs/Controller/RegisterController.java
+++ src/main/java/org/lbs/Controller/RegisterController.java
@@ -1,10 +1,34 @@
 package org.lbs.Controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import org.lbs.Model.User;
 import javafx.stage.Stage;
+import org.lbs.dao.UserDAO;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
 
 public class RegisterController {
-    Stage stage;
-    User user;
+    private Stage stage;
+    private User user;
+    private UserDAO userDAO;
 
+    @FXML private Button backBtn;
+
+
+    public void backToHome () throws NullPointerException {
+
+        try {
+            FXMLLoader fxml = new FXMLLoader(getClass().getResource("/org/lbs/view/loginView.fxml"));
+            Parent parent = fxml.load();
+            backBtn.getScene().setRoot(parent);
+            System.out.println("Scene successfully set.");
+        } catch (NullPointerException e) {
+            System.out.println("The page that you are going to does not exist.");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
